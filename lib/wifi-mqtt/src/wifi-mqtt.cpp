d5817d7639712b1d4bc768f9e91b18161ee51ea9
Alex Yevtushenko <a@qx.zone>
2022-11-03 13:00:25 +0200

    Add WiFi support Add MQTT support

---
+++ lib/wifi-mqtt/src/wifi-mqtt.cpp
@@ -0,0 +1,66 @@
+#include "wifi-mqtt.h"
+
+WiFiClient wifiClient;
+PubSubClient pubSubClient(wifiClient);
+
+unsigned long 
+  lastWifiReconnect = 0,
+  lastPubSubReconnectAttempt = 0,
+  lastWifiOnline = 0;
+
+bool reconnectPubSub(unsigned long now) {
+  if (now - lastPubSubReconnectAttempt > MQTT_RECONNECT_MILLIS) {
+    lastPubSubReconnectAttempt = now;
+
+    if (pubSubClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD, MQTT_STATUS_TOPIC, MQTTQOS0, true, MQTT_STATUS_OFFLINE_MSG, true)) {
+      pubSubClient.publish(MQTT_STATUS_TOPIC, MQTT_STATUS_ONLINE_MSG, true);
+
+#ifdef VERSION
+      pubSubClient.publish(MQTT_VERSION_TOPIC, VERSION, true);
+#endif
+    }
+    
+    return pubSubClient.connected();
+  }
+
+  return false;
+}
+
+bool mqtt_loop(unsigned long now) {
+  if (!pubSubClient.connected() && !reconnectPubSub(now)) {
+    return false;
+  }
+
+  return pubSubClient.loop();
+}
+
+bool wifi_loop(unsigned long now) {
+  if (WiFi.status() != WL_CONNECTED) {
+    if (now - lastWifiOnline > WIFI_WATCHDOG_MILLIS) esp_restart(); //restart(RESET_ON_WIFI_WD_TIMEOUT);
+    else if (now - lastWifiReconnect > WIFI_RECONNECT_MILLIS) {
+      lastWifiReconnect = now;
+
+      if (WiFi.reconnect()) {
+        lastWifiOnline = now;
+        return true;
+      }
+    }
+
+    return false;
+  }
+  
+  lastWifiReconnect = now;
+  lastWifiOnline = now;
+  
+  return mqtt_loop(now);
+}
+
+void wifi_setup() {
+  WiFi.setHostname(WIFI_HOSTNAME);
+  WiFi.setAutoConnect(true);
+  WiFi.setAutoReconnect(true);
+  WiFi.setSleep(wifi_ps_type_t::WIFI_PS_NONE);
+  WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
+
+  pubSubClient.setServer(MQTT_SERVER_NAME, MQTT_SERVER_PORT);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,14 @@
 #include <Arduino.h>
 #include <esp_task_wdt.h>
+#include <wifi-mqtt.h>
+#include <ArduinoOTA.h>
 
 #define WIFI_SSID                     "qx.zone"
 #define WIFI_PASSPHRASE               "1234Qwer-"
-#define WIFI_RECONNECT_MILLIS         10000
-#define WIFI_WATCHDOG_MILLIS          60000
-
-#ifndef WIFI_HOSTNAME
-#define WIFI_HOSTNAME                 "esp32-ups-01"
-#endif
-
-#define MQTT_SERVER_NAME              "10.9.9.96"
-#define MQTT_SERVER_PORT              1883
-#define MQTT_USERNAME                 NULL
-#define MQTT_PASSWORD                 NULL
-#define MQTT_RECONNECT_MILLIS         5000
-
-#ifndef MQTT_CLIENT_ID
-#define MQTT_CLIENT_ID                WIFI_HOSTNAME
-#endif
 
 #define INT_LED                       15
+
+#define BATTERY_VOLTAGE_UPDATE_MS     5000
 
 #define AC_DETECTOR_PIN               4
 #define AC_DETECTOR_TIMEOUT_MS        (1000 / 50 / 2) * 2 + 3    // allow to miss 2 zero crosses @50Hz, +3ms
@@ -28,6 +16,11 @@
 
 #define RELAY_INV_PIN                 16
 #define RELAY_CHRG_PIN                17
+#define BATTERY_VOLTAGE_PIN           2
+#define H35_SCL                       35
+#define H35_SDA                       33
+#define H36_PIN1                      13
+#define H36_PIN2                      14
 
 typedef enum : uint8_t {
   LINE = 0,
@@ -37,7 +30,11 @@ typedef enum : uint8_t {
 unsigned long 
   now = 0,
   lastAcValue = 0,
+  lastBatteryVoltageReadMs = 0,
   zc = 0;
+
+uint16_t
+  batteryLevel = 0;
 
 ups_mode_t 
   current_mode = LINE;
@@ -45,6 +42,12 @@ ups_mode_t
 void IRAM_ATTR acDetectorISR() {
   zc++;
   lastAcValue = now;
+}
+
+void on_ota_begin() {
+  if (current_mode != LINE) {
+
+  }
 }
 
 void ac_setup() {
@@ -56,11 +59,20 @@ void setup() {
   pinMode(RELAY_INV_PIN, OUTPUT_OPEN_DRAIN);
   pinMode(RELAY_CHRG_PIN, OUTPUT_OPEN_DRAIN);
   pinMode(INT_LED, OUTPUT);
+  pinMode(BATTERY_VOLTAGE_PIN, INPUT);
 
   digitalWrite(RELAY_INV_PIN, HIGH);
   digitalWrite(RELAY_CHRG_PIN, HIGH);
 
   ac_setup();
+  log_i("AC DETECTOR SETUP COMPLETE!");
+
+  wifi_setup();
+  log_i("WIFI SETUP COMPLETE!");
+
+  ArduinoOTA.begin();
+  log_i("ARDUINO-OTA SETUP COMPLETE!");
+
   log_i("SETUP COMPLETE!");
 
   // digitalWrite(INT_LED, HIGH);
@@ -88,6 +100,8 @@ void ac_loop() {
       digitalWrite(INT_LED, HIGH);
       digitalWrite(RELAY_CHRG_PIN, HIGH); // Switch CHARGER OFF
       digitalWrite(RELAY_INV_PIN, LOW); // Switch INVERTER ON
+
+      pubSubClient.publish(MQTT_CLIENT_ID "/mode", "battery");
     }
 
     // auto value = zc;
@@ -102,7 +116,18 @@ void ac_loop() {
       digitalWrite(INT_LED, LOW);
       // digitalWrite(RELAY_CHRG_PIN, HIGH); // Switch CHARGER OFF
       digitalWrite(RELAY_INV_PIN, HIGH); // Switch INVERTER OFF
+
+      pubSubClient.publish(MQTT_CLIENT_ID "/mode", "line");
     }
+  }
+}
+
+void battery_voltage_loop() {
+  if (now - lastBatteryVoltageReadMs > BATTERY_VOLTAGE_UPDATE_MS) {
+    lastBatteryVoltageReadMs = now;
+
+    batteryLevel = analogRead(BATTERY_VOLTAGE_PIN);
+    pubSubClient.publish(MQTT_CLIENT_ID "/battery/raw", String(batteryLevel).c_str());
   }
 }
 
@@ -111,6 +136,12 @@ void loop() {
 
   now = millis();
   ac_loop();
+  battery_voltage_loop();
+  if (wifi_loop(now)) {
+    if (current_mode == LINE) {
+      ArduinoOTA.handle();
+    }
+  }
 
   delay(1);
 }
